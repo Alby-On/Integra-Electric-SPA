6d542a5930a2b5965df0d4ad19593a29318ee237
Alby-On <66146586+Alby-On@users.noreply.github.com>
2026-03-12 12:41:15 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+function incluirComponentes() {
+            // Cargar Header
+            fetch('header.html')
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('header-placeholder').innerHTML = data;
+                });
 
+            // Cargar Footer
+            fetch('footer.html')
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('footer-placeholder').innerHTML = data;
+                });
+        }
+
+        // Ejecutar la función al cargar la página
+        window.onload = incluirComponentes;
